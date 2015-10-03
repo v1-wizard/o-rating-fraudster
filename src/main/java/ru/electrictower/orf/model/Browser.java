a80cf6739c3cb07b5d659dde83e626b93e4bf2ed
v1-wizard <1578894265w@gmail.com>
2015-10-04 00:46:50 +0300

    No application use phantomJs driver. Up version webdriver. Fix login issue.

--- src/main/java/ru/electrictower/orf/model/Browser.java
+++ src/main/java/ru/electrictower/orf/model/Browser.java
@@ -22,7 +22,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.electrictower.orf.beans.Article;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -30,62 +32,62 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 /**
  * @author Aliaksei Boole
  */
-public class Browser
-{
+public class Browser {
     public WebDriver webDriver;
     public boolean isLogin;
 
-    public Browser()
-    {
+    public Browser() {
         initWebDriver();
     }
 
-    private void initWebDriver()
-    {
-        webDriver = new HtmlUnitDriver();
-        webDriver.manage().timeouts().implicitlyWait(10, SECONDS);
+    private void initWebDriver() {
+        webDriver = getWebDriver();
+        webDriver.manage().timeouts().implicitlyWait(20, SECONDS);
     }
 
-    public void login(String userName, String password)
-    {
-        webDriver.get("https://profile.onliner.by/login");
-        WebElement userNameField = webDriver.findElement(By.id("username"));
+    public void login(String userName, String password) {
+        webDriver.get("https://profile.onliner.by/");
+        WebElement userNameField = webDriver.findElement(By.xpath("//input[@type='text']"));
         userNameField.clear();
         userNameField.sendKeys(userName);
-        WebElement passwordField = webDriver.findElement(By.id("password"));
+        WebElement passwordField = webDriver.findElement(By.xpath("//input[@type='password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
-        WebElement checkBox = webDriver.findElement(By.name("autologin"));
-        checkBox.click();
-        WebElement loginButton = webDriver.findElement(By.cssSelector(".btn>input"));
+        WebElement loginButton = webDriver.findElement(By.xpath("//button[contains(@class,'auth-box__auth-submit')]"));
         loginButton.click();
-        try
-        {
-            WebElement accountInformation = webDriver.findElement(By.xpath("//a[contains(@class,'exit')]"));
+        try {
+            WebElement accountInformation = webDriver.findElement(By.xpath("//a[@class='exit']"));
             isLogin = accountInformation.isDisplayed();
-        }
-        catch (NoSuchElementException e)
-        {
+        } catch (NoSuchElementException e) {
             isLogin = false;
         }
     }
 
-    public void sendComment(String text, Article article)
-    {
+    public void sendComment(String text, Article article) {
         webDriver.get(article.getCommentUrl());
         webDriver.findElement(By.xpath("//textarea[@name='message']")).sendKeys(text);
         webDriver.findElement(By.xpath("//button[@class='btn-2-green btn-2-green-send']")).click();
     }
 
-    public String getLoginError()
-    {
+    public String getLoginError() {
         WebElement errorElem = webDriver.findElement(By.xpath("//div[@class='error']/p"));
         return errorElem.getText();
     }
 
-    public boolean isLogin()
-    {
+    public boolean isLogin() {
         return isLogin;
+    }
+
+
+    private static WebDriver getWebDriver() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability("takesScreenshot", true);
+        caps.setCapability(
+                PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                "path_to_phantom_js_driver"
+        );
+        return new PhantomJSDriver(caps);
     }
 
 }
